6535d522d76d2a6be47dad5b0dc70f01fd664236
trueSlav <2001idercach@gmail.com>
2023-12-20 19:05:37 +0300

    A folder/ A catalog

---
+++ constants/catalog.js
@@ -0,0 +1,40 @@
+// Имитация каталога товара. Такой код должен приходить с сервера.
+// Реализовать в будущем запрос.
+const CATALOG = [
+	{
+		id: 'el1',
+		name: 'ARIA PRO II STG-003SPL VW',
+		img: 'img/catalog/aria-el1.jpeg', // Файл храниться сокально.
+		price: '15379'
+	},
+	{
+		id: 'el2',
+		name: 'ARIA PRO II TEG-002 BK',
+		img: 'img/catalog/aria-el2.jpeg',
+		price: '18171'
+	},
+	{
+		id: 'el3',
+		name: 'ARIA PRO II MAC-STD MBK',
+		img: 'img/catalog/aria-el3.jpeg',
+		price: '25399'
+	},
+	{
+		"id": "el4",
+		"name": "EPIPHONE LES PAUL STUDIO LT EBONY",
+		"img": "https://i.ibb.co/4Tg91WL/4.jpg", // Ссылка точнее имитирует файл пришедший с сервера.
+		"price": 19900
+},
+{
+		"id": "el5",
+		"name": "FENDER SQUIER BULLET TREM HSS AWT",
+		"img": "https://i.ibb.co/MC7sy1F/5.jpg",
+		"price": 14500
+},
+{
+		"id": "el6",
+		"name": "FENDER SQUIER BULLET MUSTANG HH BLK",
+		"img": "https://i.ibb.co/qNBWPb0/6.jpg",
+		"price": 14600
+},
+]
